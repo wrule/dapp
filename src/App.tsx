400b559ded5c2e3df9a55d9ec9eaa704433ac24d
jimao <jimao@jimaodeMacBook-Pro.local>
2023-03-30 18:19:44 +0800

    提交代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.scss';
+import { Layout } from 'antd';
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <span>你好，世界</span>
-    </div>
+    <Layout>
+      <Header>Header</Header>
+      <Content>你好，世界</Content>
+    </Layout>
   );
 }
 
